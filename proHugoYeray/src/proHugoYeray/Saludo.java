a009eaf0b889e62897c820ce84a49711abc8fa2f
Yeray <yerayliloaiza@gmail.com>
2021-04-11 19:46:59 +0200

    annadida variable anno

--- proHugoYeray/src/proHugoYeray/Saludo.java
+++ proHugoYeray/src/proHugoYeray/Saludo.java
@@ -3,5 +3,9 @@ package proHugoYeray;
 public class Saludo {
 	public static void main (String[] args) {
 		System.out.println("Hola, estoy usando Git!");
+		
+		int anno = 2021;
+		
+		System.out.println("Estamos en el año " + anno + ".");
 	}
 }
